2c766b73249e63d41b22fb901c52e5979d5224b5
Ivarsky <iamh90@gmail.com>
2023-04-17 19:48:59 +0000

    control inputs

--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -1,27 +1,57 @@
+import { useState } from 'react';
 import Button from '../shared/Button';
 import { login } from './service';
 
 // eslint-disable-next-line react/prop-types
 function LoginPage({ onLogin }) {
+  // eslint-disable-next-line no-undef
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: '',
+  });
+
   const handleSubmit = async event => {
     event.preventDefault();
 
-    await login({
-      username: event.target.username.value,
-      password: event.target.password.value,
-    });
+    await login({ credentials });
 
     //Estoy logueado
     onLogin();
   };
 
+  const handleChange = event => {
+    console.log(event.target.value, event.target.name);
+
+    // if (event.target.name === 'username'){
+    //   setCredentials({...credentials, username: event.target.value});
+    // }
+    // if (event.target.name === 'password') {
+    //   setCredentials({...credentials, password: event.target.value});
+    // }
+    setCredentials({ ...credentials, [event.target.name]: event.target.value });
+  };
+
+  const buttonDisabled = !credentials.username || !credentials.password;
+
   return (
     <div>
       <h1>Log in to your Account</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="username" />
-        <input type="password" name="password" />
-        <Button type="submit" variant="primary">
+        <input
+          type="text"
+          name="username"
+          onChange={handleChange}
+          autoComplete={false}
+          value={credentials.username}
+        />
+        <input
+          type="password"
+          name="password"
+          onChange={handleChange}
+          autoComplete={false}
+          value={credentials.password}
+        />
+        <Button type="submit" variant="primary" disabled={buttonDisabled}>
           Log In
         </Button>
       </form>
