344e89b083d21ad1937e102e70a2866333f7806c
Ivarsky <iamh90@gmail.com>
2023-04-19 20:50:06 +0000

    advert details params

--- src/components/adverts/AdvertPage.js
+++ src/components/adverts/AdvertPage.js
@@ -1,9 +1,14 @@
+import { useParams } from 'react-router-dom';
 import Layout from '../Layout/Layout';
 
 const AdvertPage = props => {
+  const params = useParams();
   return (
     <Layout title="Advert detail" {...props}>
-      <div>Advert Detail</div>
+      <div>
+        Advert Detail
+        {params.advertId}
+      </div>
     </Layout>
   );
 };
