4d5bde59b4dde66bb1a76af95d0907a7dab0ce72
Ivarsky <iamh90@gmail.com>
2023-06-12 21:12:18 +0000

    refactor:  react redux provador and browser router to root.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import App from './App';
 import storage from './utils/storage';
 import { setAuthorizationHeader } from './api/client';
 import configureStore from './redux';
-import { BrowserRouter } from 'react-router-dom';
 import { AuthContextProvider } from './components/auth/context';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Root from './Root';
 
 const accessToken = storage.get('auth');
 if (accessToken) {
@@ -18,10 +18,10 @@ const store = configureStore();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Root store={store}>
       <AuthContextProvider isInitiallyLogged={!!accessToken}>
         <App />
       </AuthContextProvider>
-    </BrowserRouter>
+    </Root>
   </React.StrictMode>,
 );
