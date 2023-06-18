fc348657aea3305cb3ef901802948b25f95dabd5
Ivarsky <iamh90@gmail.com>
2023-06-18 14:22:40 +0000

    test reducer

---
+++ src/store/__tests__/reducers.js
@@ -0,0 +1,32 @@
+import { authLoginSucces, authLogout } from "../actions";
+import { auth, defaultState } from "../reducers";
+
+describe("auth", () => {
+  test('should manage "AUTH_LOGIN_SUCCESS" action', () => {
+    const state = defaultState.auth;
+    const action = authLoginSucces();
+    const newState = auth(state, action);
+    expect(newState).toBe(true);
+  });
+
+  test('should manage "AUTH_LOGOUT" action', () => {
+    const state = defaultState.auth;
+    const action = authLogout();
+    const newState = auth(state, action);
+    expect(newState).toBe(false);
+  });
+
+  test("should manage any other action when state is undefined", () => {
+    const state = undefined;
+    const action = { type: "ANY" };
+    const newState = auth(state, action);
+    expect(newState).toBe(defaultState.auth);
+  });
+
+  test("should manage any other action when state is not undefined", () => {
+    const state = true;
+    const action = { type: "ANY" };
+    const newState = auth(state, action);
+    expect(newState).toBe(state);
+  });
+});
